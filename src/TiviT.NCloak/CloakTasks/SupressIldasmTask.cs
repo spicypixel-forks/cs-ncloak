c52aa81ed48209749c2759304ef22d1f47cc3998
Aaron Oneal <aaron.oneal@spicypixel.com>
2013-06-03 16:06:58 -0700

    Fix duplicate mscorlib caused by SupressIldasmTask.
    Everywhere that NCloak uses typeof(System.*) needs to be updated to use the target module's mscorlib version instead of the framework version running NCloak. This is only the first task to be fixed.

--- src/TiviT.NCloak/CloakTasks/SupressIldasmTask.cs
+++ src/TiviT.NCloak/CloakTasks/SupressIldasmTask.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Mono.Cecil;
-using System;
 
 namespace TiviT.NCloak.CloakTasks
 {
@@ -23,6 +24,7 @@ namespace TiviT.NCloak.CloakTasks
         public void RunTask(ICloakContext context)
         {
             Dictionary<string, AssemblyDefinition> assemblyCache = context.GetAssemblyDefinitions();
+			var resolver = new DefaultAssemblyResolver();
             foreach (string assembly in assemblyCache.Keys)
             {
                 AssemblyDefinition def = assemblyCache[assembly];
@@ -40,8 +42,11 @@ namespace TiviT.NCloak.CloakTasks
                 //Only add if it's not there already
                 if (found == null)
                 {
-                    //Add one
-                    MethodReference constructor = def.MainModule.Import(typeof (SuppressIldasmAttribute).GetConstructor(Type.EmptyTypes));
+                    //Add one (using target module's mscorlib)
+					var mscorlibRef = def.MainModule.AssemblyReferences.Where(a => a.Name.Contains("mscorlib")).Single();
+					var mscorlib = resolver.Resolve(mscorlibRef);
+					var supressAttrDef = mscorlib.MainModule.GetType("System.Runtime.CompilerServices.SuppressIldasmAttribute");
+					MethodReference constructor = def.MainModule.Import(supressAttrDef.Methods.First(m => m.IsConstructor && !m.HasParameters));
                     CustomAttribute attr = new CustomAttribute(constructor);
                     def.CustomAttributes.Add(attr);
                 }
